cc5d75fa48f6f0ad43da55f77197ce06b80d50f3
rn-mgl <reinb2ra@gmail.com>
2026-03-05 21:31:50 +0800

    deps

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Hello" });
+  return res.json({ message: "Hello", success: true, object: "test 2s" });
 });
 
 app.use(express.json());
